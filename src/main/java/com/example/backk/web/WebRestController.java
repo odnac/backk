2ad654d8cd6acf378ebff40612148e27a60db8d9
odnac <cancode777@gmail.com>
2022-05-17 20:34:11 +0900

    3-1-2 SpringBoot & Handlebars 게시글 등록(2)

--- src/main/java/com/example/backk/web/WebRestController.java
+++ src/main/java/com/example/backk/web/WebRestController.java
@@ -2,6 +2,7 @@ package com.example.backk.web;
 
 import com.example.backk.domain.posts.PostsRepository;
 import com.example.backk.dto.posts.PostsSaveRequestDto;
+import com.example.backk.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class WebRestController {
 
-    private static final Logger logger = LoggerFactory.getLogger(MetricsProperties.Web.class);
-    private PostsRepository postsRepository;
+    //private static final Logger logger = LoggerFactory.getLogger(MetricsProperties.Web.class);
+    //private PostsRepository postsRepository;
+    private PostsService postsService;
 
     @GetMapping("/hello")
     public String hello() {
@@ -24,8 +26,9 @@ public class WebRestController {
     }
 
     @PostMapping("/posts")
-    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
-        logger.info(dto.toString());
-        postsRepository.save(dto.toEntity());
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto) {
+        //logger.info(dto.toString());
+        //postsRepository.save(dto.toEntity());
+        return postsService.save(dto);
     }
 }
