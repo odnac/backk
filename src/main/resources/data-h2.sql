1585788a2a734e46a90a2ee3fee7809677c464e4
odnac <cancode777@gmail.com>
2022-05-21 17:57:07 +0900

    3-2 SpringBoot & Handlebars 게시글 목록오류 L

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,2 @@
+insert into posts (title, author, content, created_date, modified_date) vales('테스트1', 'devzec', '테스트1의 본문', now(), now());
+insert into posts (title, author, content, created_date, modified_date) vales('테스트2', 'devzec', '테스트2의 본문', now(), now());
